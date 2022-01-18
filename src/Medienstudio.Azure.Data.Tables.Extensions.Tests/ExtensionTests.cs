e4427e83e14842154f844069e4a3a4d5afe82150
Thomas Pentenrieder <pentenrieder@hotmail.com>
2022-01-18 16:55:36 +0100

    added GetAllEntitiesAsync, AddEntitiesAsync, DeleteAllEntitiesAsync

--- src/Medienstudio.Azure.Data.Tables.Extensions.Tests/ExtensionTests.cs
+++ src/Medienstudio.Azure.Data.Tables.Extensions.Tests/ExtensionTests.cs
@@ -1,6 +1,7 @@
 using Azure.Data.Tables;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Medienstudio.Azure.Data.Tables.Extensions.Tests
@@ -12,28 +13,99 @@ namespace Medienstudio.Azure.Data.Tables.Extensions.Tests
         private const string AccountName = "devstoreaccount1";
         private const string AccountKey = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
         private const string TableEndpoint = "http://127.0.0.1:10002/devstoreaccount1";
-        private const string ConnectionString = $"DefaultEndpointsProtocol={DefaultEndpointsProtocol};AccountName={AccountName};AccountKey={AccountKey};TableEndpoint={TableEndpoint};";
-        
-        private TableClient _tableClient = new(connectionString: ConnectionString, tableName: "TestTable");
+        //private const string ConnectionString = $"DefaultEndpointsProtocol={DefaultEndpointsProtocol};AccountName={AccountName};AccountKey={AccountKey};TableEndpoint={TableEndpoint};";
+        private const string ConnectionString = $"DefaultEndpointsProtocol=https;AccountName=tablestoragetest;AccountKey=YAYsqjyQGwO2ngoBEhiifgSc0JNgQLToVOidIcELAUaVS1EUN4iIMowIZCCcr2VTQRunB3huRjjKK1sFHrDswg==;EndpointSuffix=core.windows.net";
+
+
+        private TableClient? _tableClient;
+
+        private static string RandomTableName()
+        {
+            return "t" + Guid.NewGuid().ToString("N");
+        }
+
+        private void CreateTestData()
+        {
+            if (_tableClient is null)
+                return;
+
+            List<TableTransactionAction> batch = new();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var e = new TableEntity()
+                {
+                    PartitionKey = "123",
+                    RowKey = Guid.NewGuid().ToString()
+                };
+                e.Add("Test", Guid.NewGuid().ToString());
+                batch.Add(new TableTransactionAction(TableTransactionActionType.Add, e));
+            }
+
+            _tableClient.SubmitTransaction(batch);
+
+            _tableClient.UpsertEntity(new TableEntity() { PartitionKey = "1", RowKey = "2" });
+            _tableClient.UpsertEntity(new TableEntity() { PartitionKey = "2", RowKey = "2" });
+            _tableClient.UpsertEntity(new TableEntity() { PartitionKey = "3", RowKey = "4" });
+        }
 
         [TestInitialize]
         public void Initialize()
         {
+            _tableClient = new(connectionString: ConnectionString, tableName: RandomTableName());
             _tableClient.CreateIfNotExists();
-            _tableClient.UpsertEntity(new TableEntity() { PartitionKey= "1", RowKey= "2" });
-            _tableClient.UpsertEntity(new TableEntity() { PartitionKey = "2", RowKey = "2" });
-            _tableClient.UpsertEntity(new TableEntity() { PartitionKey = "3", RowKey = "4" });
-
         }
 
         [TestMethod]
-        public async Task GetByRowKeyTest()
+        public async Task GetAllEntitiesAsyncTest()
         {
-            var rows = await _tableClient.GetByRowKeyAsync<TableEntity>("2");
+            CreateTestData();
+
+            var rows = await _tableClient.GetAllEntitiesAsync<TableEntity>();
+            Assert.AreEqual(103, rows.Count);
+        }
+
+        [TestMethod]
+        public async Task GetEntitiesByRowKeyAsyncTest()
+        {
+            CreateTestData();
+
+            var rows = await _tableClient.GetAllEntitiesByRowKeyAsync<TableEntity>("2");
             Assert.AreEqual(2, rows.Count);
 
-            var rows2 = await _tableClient.GetByRowKeyAsync<TableEntity>(Guid.NewGuid().ToString());
+            var rows2 = await _tableClient.GetAllEntitiesByRowKeyAsync<TableEntity>(Guid.NewGuid().ToString());
             Assert.AreEqual(0, rows2.Count);
+        }
+
+        [TestMethod]
+        public async Task AddAllEntitiesAsyncTest()
+        {
+            List<TableEntity> entities = new();
+            for (int i = 0; i < 1000; i++)
+            {
+                var e = new TableEntity()
+                {
+                    PartitionKey = (i % 20).ToString(),
+                    RowKey = Guid.NewGuid().ToString()
+                };
+                entities.Add(e);
+            }
+            await _tableClient.AddEntitiesAsync(entities);
+        }
+
+        [TestMethod]
+        public async Task DeleteAllEntitiesAsyncTest()
+        {
+            CreateTestData();
+            await _tableClient.DeleteAllEntitiesAsync();
+            var remaining = await _tableClient.GetAllEntitiesAsync<TableEntity>();
+            Assert.AreEqual(0, remaining.Count);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _tableClient?.Delete();
         }
     }
 }
--- src/Medienstudio.Azure.Data.Tables.Extensions/Extensions.cs
+++ src/Medienstudio.Azure.Data.Tables.Extensions/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Data.Tables;
+﻿using Azure;
+using Azure.Data.Tables;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,16 +12,79 @@ namespace Medienstudio.Azure.Data.Tables.Extensions
     public static class Extensions
     {
         /// <summary>
+        /// Groups entities by PartitionKey into batches of max 100 for valid transactions
+        /// </summary>
+        private static async Task BatchManipulateEntities<T>(TableClient tableClient, IEnumerable<T> entities, TableTransactionActionType tableTransactionActionType) where T : class, ITableEntity, new()
+        {
+            var groups = entities.GroupBy(x => x.PartitionKey);
+            foreach (var group in groups)
+            {
+                var items = group.AsEnumerable();
+                while (items.Any())
+                {
+                    var batch = items.Take(100);
+                    items = items.Skip(100);
+
+                    var actions = new List<TableTransactionAction>();
+                    actions.AddRange(batch.Select(e => new TableTransactionAction(tableTransactionActionType, e)));
+                    await tableClient.SubmitTransactionAsync(actions);
+                }
+            }
+        }
+
+        /// <summary>
         /// Returns all rows for a given RowKey
         /// </summary>
-        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T">Implementation of ITableEntity</typeparam>
         /// <param name="tableClient">The authenticated TableClient</param>
         /// <param name="rowKey">The RowKey</param>
         /// <remarks>Will result in a Table Scan which can result in bad query performance</remarks>
-        /// <returns></returns>
-        public static async Task<IList<T>> GetByRowKeyAsync<T>(this TableClient tableClient, string rowKey) where T : class, ITableEntity, new()
+        /// <returns>List of all entities in the table with specified RowKey</returns>
+        public static async Task<IList<T>> GetAllEntitiesByRowKeyAsync<T>(this TableClient tableClient, string rowKey) where T : class, ITableEntity, new()
         {
-            return await tableClient.QueryAsync<T>(x => x.RowKey == rowKey).ToListAsync();
+            return await tableClient.QueryAsync<T>(x => x.RowKey == rowKey).ToListAsync().ConfigureAwait(false);
         }
+
+        /// <summary>
+        /// Returns all rows in the table
+        /// </summary>
+        /// <typeparam name="T">Implementation of ITableEntity</typeparam>
+        /// <param name="tableClient">The authenticated TableClient</param>
+        /// <returns></returns>
+        public static async Task<IList<T>> GetAllEntitiesAsync<T>(this TableClient tableClient) where T : class, ITableEntity, new()
+        {
+            return await tableClient.QueryAsync<T>(maxPerPage: 1000).ToListAsync().ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Add a list of entites with automatic batching by PartitionKey
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tableClient"></param>
+        /// <param name="entities"></param>
+        /// <param name="tableTransactionActionType"></param>
+        /// <returns></returns>
+        public static async Task AddEntitiesAsync<T>(this TableClient tableClient, IEnumerable<T> entities, TableTransactionActionType tableTransactionActionType = TableTransactionActionType.Add) where T : class, ITableEntity, new()
+        {
+            await BatchManipulateEntities(tableClient, entities, tableTransactionActionType);
+        }
+
+        /// <summary>
+        /// Deletes all rows from the table
+        /// </summary>
+        /// <param name="tableClient">The authenticated TableClient</param>
+        /// <returns></returns>
+        public static async Task DeleteAllEntitiesAsync(this TableClient tableClient)
+        {
+            // Only the PartitionKey & RowKey fields are required for deletion
+            AsyncPageable<TableEntity> entities = tableClient
+                .QueryAsync<TableEntity>(select: new List<string>() { "PartitionKey", "RowKey" }, maxPerPage: 1000);
+
+            await entities.AsPages().ForEachAwaitAsync(async page => {
+                // Since we don't know how many rows the table has and the results are ordered by PartitonKey+RowKey
+                // we'll delete each page immediately and not cache the whole table in memory
+                await BatchManipulateEntities(tableClient, page.Values, TableTransactionActionType.Delete);
+            });
+        } 
     }
 }
